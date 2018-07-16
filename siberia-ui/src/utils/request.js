70b008b4975a2a1a022683eb238c567e85e39a22
dan <i@shanhh.com>
2018-07-16 14:29:12 +0800

    feature 统一异常处理

--- siberia-ui/src/utils/request.js
+++ siberia-ui/src/utils/request.js
@@ -24,11 +24,15 @@ function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
   }
-  const errortext = codeMessage[response.status] || response.statusText;
-  notification.error({
-    message: `请求错误 ${response.status}: ${response.url}`,
-    description: errortext,
+  let errortext = codeMessage[response.status] || response.statusText;
+  response.json().then(body => {
+    errortext = body.detail || errortext;
+    notification.error({
+      message: `请求错误 ${response.status}: ${response.url}`,
+      description: errortext,
+    });
   });
+
   const error = new Error(errortext);
   error.name = response.status;
   error.response = response;
